db0d1a131ae4ba853173f22ec4d0c64c956e4a8b
TR BURNS <trburns@TRs-MacBook-Air.local>
2026-04-08 19:42:14 -0400

    fix: remove TLS line from before imports

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
